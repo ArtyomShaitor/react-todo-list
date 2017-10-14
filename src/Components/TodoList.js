4949448fd4339304bacf547319707d70071ee32f
Artyom Shaitor <artyomshaitor@gmail.com>
2017-10-14 20:05:29 +0000

    updating key events

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -3,6 +3,8 @@ import TodoListItem from './TodoListItem';
 import { connect } from 'react-redux';
 import Actions from "../Actions";
 
+const ENTER_BUTTON = 'Enter';
+
 class TodoList extends Component
 {
     addNewTask = () =>
@@ -13,11 +15,18 @@ class TodoList extends Component
         }
     };
 
+    handleKeyPress = (event) =>
+    {
+        if (event.key === ENTER_BUTTON) {
+            this.addNewTask();
+        }
+    };
+
     render() {
         return (
             <div className="todolist">
                 <div className="todolist__new-todo">
-                    <input className="todolist__new-todo-input" ref={ (input) => { this.newTaskInput = input }} placeholder="New task here..." />
+                    <input className="todolist__new-todo-input" onKeyPress={this.handleKeyPress} ref={ (input) => { this.newTaskInput = input }} placeholder="New task here..." />
                     <button className="todolist__new-todo-add" onClick={this.addNewTask}>add</button>
                 </div>
                 <div className="todolist__container">
