025e747adf1ea0c2f6f1363c58f15611e8cc765b
Artyom Shaitor <artyomshaitor@gmail.com>
2017-10-20 22:28:21 +0300

    changes in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,27 @@ const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 
-
-gulp.task('build', function () {
-    return gulp.src('src/resources/css/**/*.scss')
+let configure = (gulp, name = 'test.css') =>
+{
+    return gulp
+        .src('src/resources/css/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(concat('all.css'))
+        .pipe(concat(name))
         .pipe(autoprefixer({
             browsers : ['last 20 versions', 'Firefox < 20']
         }))
         .pipe(gulp.dest('public/'));
+}
+
+
+
+
+gulp.task('build', function () {
+    return configure(gulp, 'all.css');
 });
 
 gulp.task('watch', function () {
     gulp.watch('src/resources/css/**/*.scss', function(){
-        return gulp.src('src/resources/css/**/*.scss')
-            .pipe(sass().on('error', sass.logError))
-            .pipe(concat('test.css'))
-            .pipe(autoprefixer({
-                browsers : ['last 20 versions', 'Firefox < 20']
-            }))
-            .pipe(gulp.dest('public/'));
+        return configure(gulp, 'test.css');
     });
 });
